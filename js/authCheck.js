c9f86fd19824f9c304c9884e2c42dc61980e0536
guy-shavit <guy.shavit.g@gmail.com>
2024-03-11 18:41:11 +0200

    fix logout_2.0

--- js/authCheck.js
+++ js/authCheck.js
@@ -30,7 +30,7 @@ function displayLoggedInUsers() {
             });
         } else {
             // User is not signed in, redirect to login page
-            window.location.href = 'login.html';
+            window.location.href = 'index.html';
         }
     });
 }
@@ -46,7 +46,7 @@ function logout() {
             .then(() => {
                 console.log("User logged out successfully");
                 // Redirect or perform other actions after logout
-                window.location.href = 'login.html';
+                window.location.href = 'index.html';
             })
             .catch((error) => {
                 console.error("Logout error", error.message);
